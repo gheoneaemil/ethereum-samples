2398d6e1ab9746c9ad8b75d307e64331e290ede8
Gheonea Emilian <32714268+gheoneaemil@users.noreply.github.com>
2022-02-23 13:48:43 +0200

    Update mnemonicToPrivateKey.js

--- web3/accounts/mnemonicToPrivateKey.js
+++ web3/accounts/mnemonicToPrivateKey.js
@@ -1,30 +1,23 @@
 /*
  * @DIIMIIM
  * 15 dec 2021
- * This code sample will use a mnemonic passphrase to generate a serie of possible associated accounts
+ * This code sample will use a mnemonic passphrase to generate the associated account
  */
 const bip39 = require("bip39");
 const { hdkey } = require('ethereumjs-wallet');
 
-
-// Normally you should get the good account on the first position. If not, you can extend the length of output accounts
-const count = 1;
-
 // Add here the mnemonic passphrase
 const mnemonic = "MNEMONIC_PASSPHRASE_HERE";
 
-function generateAddressesFromSeed(mnemonic, count) {  
+function generateAddressesFromSeed(mnemonic) {  
     const seed = bip39.mnemonicToSeedSync(mnemonic);
     const hdwallet = hdkey.fromMasterSeed(seed);
     const wallet_hdpath = "m/44'/60'/0'/0/";
-
-    for (let i = 0; i < count; i++) {
-      let wallet = hdwallet.derivePath(wallet_hdpath + i).getWallet();
-      let address = "0x" + wallet.getAddress().toString("hex");
-      let privateKey = wallet.getPrivateKey().toString("hex");
-      console.log("Address: " + address + "\nPrivate key: " + privateKey);
-    }
+    const wallet = hdwallet.derivePath(wallet_hdpath + i).getWallet();
+    const address = "0x" + wallet.getAddress().toString("hex");
+    const privateKey = wallet.getPrivateKey().toString("hex");
+    console.log("Address: " + address + "\nPrivate key: " + privateKey);
 }
 
 
-generateAddressesFromSeed(mnemonic, count);
+generateAddressesFromSeed(mnemonic);
